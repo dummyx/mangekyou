12a3c175608ca58e8a5dfe3d43c12609f89797ce
dummyx <40601688+dummyx@users.noreply.github.com>
2020-05-30 09:48:30 +0900

    Adding Windows workaround

--- b4ke.py
+++ b4ke.py
@@ -1,20 +1,26 @@
-from pathlib import Path
+# -*- coding: UTF-8 -*-
+import os
 import subprocess
 import sys
 from fractions import Fraction
+from tkinter.filedialog import askopenfilename
+from tkinter import Tk
 
-preset = 'veryslow'
+preset = 'slower'
 crf = '14.5'
 
 def b4ke(video_file,subtitle_file):
-    output_file = '[BE4K20K]'+video_file.name[:video_file.name.rfind('.')]+'.mp4'
+    if os.name=='nt':
+        subtitle_file = subtitle_file.replace(':','\\:')
+    basename = os.path.basename(video_file)
+    output_file = '[BE4K20K]'+basename[:basename.rfind('.')]+'.mp4'
     keyint = int(get_framerate(video_file))*10
     ffmpeg_arg = ['ffmpeg',
                           '-i',str(video_file),
                           '-c:a','aac',
                           '-ar','44100',
                           '-b:a','320k',
-                          '-vf','ass=%s'%str(subtitle_file),
+                          '-filter_complex','subtitles=\'%s\''%subtitle_file,
                           '-c:v','libx264',
                           '-preset',preset,
                           '-crf', crf,
@@ -35,12 +41,11 @@ def get_framerate(video_file):
 
 if __name__=='__main__':
     if len(sys.argv)==3:
-        video_file = Path(sys.argv[1])
-        subtitle_file = Path(sys.argv[2])
+        video_file = sys.argv[1]
+        subtitle_file = sys.argv[2]
     else:
-        vid = input('拖拽视频文件或输入路径\n')
-        sub = input('拖拽字幕文件或输入路径\n')
-        video_file = Path(vid)
-        subtitle_file = Path(sub)
+        Tk().withdraw()
+        video_file = askopenfilename(filetypes=[('MP4 video file','.mp4')],title='Select video file')
+        subtitle_file = askopenfilename(filetypes=[('ASS subtitle file','.ass')],title='Select subtitle file')
     
     b4ke(video_file,subtitle_file)
