5825f3bd7c3574a78a0f7c4d3258ac0ac2a03dfc
dummyx <40601688+dummyx@users.noreply.github.com>
2020-06-14 12:35:42 +0900

    Using native tar instead

--- vac.py
+++ vac.py
@@ -1,19 +1,34 @@
-import subprocess
+#import subprocess
 import zipfile
+import tarfile
 import sys
 import os
+import io
 
 if __name__ == '__main__':
-    p7z_args = ['7za', 'a', '-t7z', '-m0=lzma2', '-mx=9', '-mfb=64', '-md=32m']
+    #p7z_args = ['7za', 'a', '-si','-t7z', '-m0=lzma2', '-mx=9', '-mfb=64', '-md=32m']
 
 
     original_file_path = sys.argv[1]
-    output_file_path = original_file_path[:original_file_path.rfind('.')]+'.7z'
+    output_file_path = original_file_path[:original_file_path.rfind('.')]+'.tar.xz'
     
     original_file = zipfile.ZipFile(original_file_path)
-    name_list = original_file.namelist()
-    for f in name_list:
-        original_file.extract(f)
-        subprocess.run(p7z_args+[output_file_path,f])
-        if not os.path.isdir(f):
-            os.remove(f)
+    info_list = original_file.infolist()
+    
+    extracted_stream = io.BytesIO()
+    tar_stream = io.BytesIO()
+    tar = tarfile.open(name=output_file_path,mode='w:xz')
+    
+    for f in info_list:
+        if f.is_dir():
+            continue
+        extracted_stream.write(original_file.read(f))
+        extracted_stream.seek(0)
+        info = tarfile.TarInfo(name=f.filename)
+        info.size = extracted_stream.getbuffer().nbytes
+        tar.addfile(info, extracted_stream)
+        extracted_stream.flush()
+
+        #subprocess.run(p7z_args+[output_file_path,f])
+        print('Added %s'%f.filename,end='\r')
+    tar.close()
